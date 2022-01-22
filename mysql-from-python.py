7e228c6acc2b0c8a29e06356c525b1911dce92ef
emmyamanda <emmysignal95@hotmail.com>
2022-01-22 19:00:58 +0000

    Update python

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 # Get username from Cloud9 workspace
@@ -14,10 +15,11 @@ connection = pymysql.connect(host='localhost',
 try:
     #Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        rows = [("Bob", 21, "1990-02-06 23:04:56"),
+                ("Jim", 56, "1955-05-09 13:12:45"),
+                ("Fred", 100, "1995-03-08 01:01:01")]
+        cursor.executemany("INSERT INTO Friends VALUES (%s, %s, %s);", rows)
+        connection.commit()
 finally:
-    #Close the connection, regardless of whether the above was successful
+    # Close the connection, regardless of whether the above was successful
     connection.close()
